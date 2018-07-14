981fc073f42a1ac0d2835aa7c59ff8d49094b8c0
nowwoo91 <nowwoo91@gmail.com>
2018-07-14 17:22:32 +0900

    jump test and save scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerController : MonoBehaviour {
+    
+    public float jumpForce = 6.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Jump() {
+        Vector2 jumping = gameObject.GetComponent<Rigidbody2D>().velocity;
+        jumping.y = transform.position.y + jumpForce;
+        //jumping.x = transform.position.x + jumpForce;
+        gameObject.GetComponent<Rigidbody2D>().velocity = jumping;
+
+        Debug.Log("jump");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "GameController")
+        {
+            Debug.Log("Game Over");
+            //
+        }
+    }
+}
--- Assets/Scripts/UserController.cs
+++ Assets/Scripts/UserController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UserController : MonoBehaviour {
+    
+    private bool touch;
+    private GameObject player;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,16 @@ public class UserController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Touch();
 	}
+
+    void Touch () {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("ddddd");
+            touch = true;
+            player = GameObject.FindWithTag("Player");
+            player.GetComponent<PlayerController>().Jump();
+        }
+    }
 }
